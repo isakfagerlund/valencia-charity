25003275afeca6cba8b1bc44add7f5ced37e7ff8
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-12-03 16:16:08 +0100

    testing file upload

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,20 +1,58 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { nanoid } from 'nanoid';
 
 import { peopleRoute } from './src/people.js';
 
 export type Bindings = {
   TURSO_URL: string;
   TURSO_TOKEN: string;
-  KINDE_DOMAIN: string;
-  KINDE_CLIENT_ID: string;
-  KINDE_SECRET: string;
-  KINDE_SITE_URL: string;
-  KINDE_POST_LOGOUT_REDIRECT_URL: string;
+  UNBOXING_PROJECT_BUCKET: R2Bucket;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 app.use('/*', cors());
+
+app.post('/upload', async (c) => {
+  const key = nanoid(10);
+  const formData = await c.req.parseBody();
+  const file = formData['file'];
+  if (file instanceof File) {
+    const fileBuffer = await file.arrayBuffer();
+    const fullName = file.name;
+    const ext = fullName.split('.').pop();
+    const path = `images/${key}.${ext}`;
+
+    try {
+      console.log(c.env.UNBOXING_PROJECT_BUCKET);
+      await c.env.UNBOXING_PROJECT_BUCKET.put(path, fileBuffer);
+      return c.json({ message: `Put ${key} successfully!` });
+    } catch (error) {
+      console.log(error);
+      return c.json({ message: 'Something went wrong' });
+    }
+  } else {
+    return c.text('Invalid file', 400);
+  }
+});
+
+app.get('images/:key', async (c) => {
+  const key = c.req.param('key');
+  console.log(key);
+  const object = await c.env.UNBOXING_PROJECT_BUCKET.get(key);
+
+  if (object === null) {
+    return new Response('Object Not Found', { status: 404 });
+  }
+
+  const headers = new Headers();
+  object.writeHttpMetadata(headers);
+  headers.set('etag', object.httpEtag);
+
+  return new Response(object.body, {
+    headers,
+  });
+});
 
 app.route('/people', peopleRoute);
 
--- apps/client/src/components/NewPeopleForm.tsx
+++ apps/client/src/components/NewPeopleForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -84,8 +84,29 @@ export function NewPeopleForm() {
     setIsSubmitting(false);
   }
 
+  async function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+    if (event?.target?.files?.[0]) {
+      const file = event?.target?.files?.[0];
+
+      const formData = new FormData();
+      formData.append('file', file);
+
+      try {
+        const res = await fetch(`${apiUrl}upload`, {
+          body: formData,
+          method: 'POST',
+        });
+
+        if (!res.ok) throw new Error('Failed to upload');
+      } catch (error) {
+        toast.error('Error when uploading file');
+      }
+    }
+  }
+
   return (
     <Form {...form}>
+      <Input onChange={handleFileChange} type="file" accept="image/*" />
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
