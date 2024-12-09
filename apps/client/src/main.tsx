c07515abf2e5b2d131c34f18d2e3ec069bed4fd5
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-12-09 19:01:14 +0100

    add posthog

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import { KindeProvider } from '@kinde-oss/kinde-auth-react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PostHogProvider } from 'posthog-js/react';
 import './i18n';
 
 // Set up a Router instance
@@ -26,16 +27,23 @@ export const queryClient = new QueryClient();
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <KindeProvider
-      clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
-      domain="https://auth.proyectounboxing.es"
-      redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URI}
-      logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URI}
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+      }}
     >
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </KindeProvider>
+      <KindeProvider
+        clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
+        domain="https://auth.proyectounboxing.es"
+        redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URI}
+        logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URI}
+      >
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </KindeProvider>
+    </PostHogProvider>
   );
 }
