f2cf3095907640a49ff72bd5135eb42646d1e703
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-12-10 10:42:54 +0100

    fix borken link

--- apps/client/src/routes/people.$id.tsx
+++ apps/client/src/routes/people.$id.tsx
@@ -86,8 +86,13 @@ function RouteComponent() {
           <p className="text-2xl">{person.name}</p>
           <div className="flex flex-col md:flex-row gap-8">
             <p className="w-full md:w-2/3">{person.description}</p>
-            <Button className="bg-brand text-black w-auto md:w-1/3 hover:bg-yellow-400/70">
-              <a href={person.wishlist_link}>Lista de necesidades</a>
+            <Button
+              asChild
+              className="bg-brand text-black w-auto md:w-1/3 hover:bg-yellow-400/70"
+            >
+              <a href={person.wishlist_link} target="_blank">
+                Lista de necesidades
+              </a>
             </Button>
           </div>
           {person.video_url && (
